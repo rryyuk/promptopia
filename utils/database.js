b8849a627ee7e94bebfa40b804cdc319fbf9363c
rryyuk <agranshul09@gmail.com>
2024-05-05 12:31:15 +0530

    fix

--- utils/database.js
+++ utils/database.js
@@ -1,23 +1,23 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// let isConnected = false;
+let isConnected = false;
 
-// export const connectTODB = async () => {
-//     mongoose.set('strictQuery', true);
-//     if(isConnected) {
-//         console.log('MongoDB connected already');
-//         return;
-//     }
-//     try {
-//         await mongoose.connect(process.nextTick.MONGODB_URI, {
-//             dbName: "share_prompt",
-//             useNewUrlParser:true, 
-//             useUnifiedTopology:true,
-//         })
-//         isConnected = true;
-//         console.log('MongoDB connected');
+export const connectTODB = async () => {
+    mongoose.set('strictQuery', true);
+    if(isConnected) {
+        console.log('MongoDB connected already');
+        return;
+    }
+    try {
+        await mongoose.connect(process.nextTick.MONGODB_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser:true, 
+            useUnifiedTopology:true,
+        })
+        isConnected = true;
+        console.log('MongoDB connected');
 
-//     } catch (error) {
-//         console.log('error')
-//     }
-// }
+    } catch (error) {
+        console.log('error')
+    }
+}
