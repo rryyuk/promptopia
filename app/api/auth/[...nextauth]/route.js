e577bb111d91cc4f77637790c6f3641873351397
rryyuk <agranshul09@gmail.com>
2024-05-05 15:13:41 +0530

    callBack fix in route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,32 +10,35 @@ const handler = NextAuth({
             ClientSecret:process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({session}) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-        session.user.id = sessionUser,_id.toString();
-        return session;
-    },
-    async signIn({profile}) {
-        try {
-            await connectTODB();
-            const userExist = await User.findOne({
-                email:profile.email
-            });
-            if(!userExist) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                })
+    callbacks: {
+        async session({session}) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
+            session.user.id = sessionUser,_id.toString();
+            return session;
+        },
+        async signIn({profile}) {
+            try {
+                await connectTODB();
+                const userExist = await User.findOne({
+                    email:profile.email
+                });
+                if(!userExist) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
         }
     }
+    
 })
 
 export {handler as GET, handler as POST};
